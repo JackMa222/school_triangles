1017a313492f9cb45819900f184a201318b85a74
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-02-12 09:24:05 +1100

    Add get sides function to class

--- triangles.py
+++ triangles.py
@@ -6,6 +6,8 @@ class Triangle:
         self.side_a, self.side_b, self.side_c = self.sides
     
     def validateSides(self):
+        if any(type(side) not in [float, int] for side in self.sides):
+            return (False, "Sides must be a number")
         if any(side <= 0 for side in self.sides):
             return (False, "Sides cannot be zero or negative")
         if self.side_a + self.side_b <= self.side_c:
@@ -25,34 +27,11 @@ class Triangle:
         else:
             triangle = "Scalene"
         return triangle
-def getSides():
-    sides = []
-    for num, i in enumerate(range(3)):
-        try:
-            sides.append(int(input(f"Input Side {num+1} ")))
-        except:
-            sys.exit("Error inputting number")
-    return sides
-
-def determineTriangle(sides):
-    for side in sides:
-        if side <= 0:
-            sys.exit("Side length needs to be greater than zero")
-        
-    sidec = max(sides)
-    sides.remove(sidec)
-    sidea = sides[0]
-    sideb = sides[1]
-
-    if (sidea == sideb) and (sideb == sidec) and (sidea == sidec):
-        triangle = "Equilateral"
-    elif (sidea == sideb) or (sideb == sidec) or (sidea == sidec):
-        triangle = "Isoceles"
-    elif ((sidea ** 2) + (sideb ** 2)) == (sidec ** 2):
-        triangle = "Right angle"
-    else:
-        triangle = "Scalene"
-    return triangle
-
-triangle_message = f"{determineTriangle(getSides())}"
-print(triangle_message)
+    
+    def getSides():
+        sides = []
+        for num in range(3):
+            try:
+                sides.append(float(input(f"Enter Side {num}")))
+            except ValueError:
+                sys.exit("Input must be a valid number")
