0b4c2d7c3d1df6cc49585c473ee2b2300b301832
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-02-12 09:34:12 +1100

    Move files and begin converting app.py to OOP pkg

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from packages.triangles import Triangle
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,30 +9,7 @@ def main():
 
 @app.route('/send', methods=["GET", "POST"])
 def calc():
-    triangleMessage = ""
     triangleError = False
     
-    sides = [float(request.form.get('sidea')), float(request.form.get('sideb')), float(request.form.get('sidec'))]
     
-    for side in sides:
-        if side <= 0:
-            triangleError = True
-            triangleMessage = "Side length cannot be zero or a negative number"
-    
-    sidec = max(sides)
-    sides.remove(sidec)
-    sidea = sides[0]
-    sideb = sides[1]
-    
-    if (sidea == sideb) and (sideb == sidec) and (sidea == sidec):
-        triangle = "Equilateral"
-    elif (sidea == sideb) or (sideb == sidec) or (sidea == sidec):
-        triangle = "Isoceles"
-    elif ((sidea ** 2) + (sideb ** 2)) == (sidec ** 2):
-        triangle = "Right angle"
-    else:
-        triangle = "Scalene"
-        
-    if triangleError is False:
-        triangleMessage = f"Triangle is {triangle}"
     return render_template("index.html", triangle_message=triangleMessage)
