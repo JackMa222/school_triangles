02f5dea734bb7de3628c674293bca71bac6d1ee7
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-02-12 09:18:14 +1100

    Add determine function triangle and error messages

--- triangles.py
+++ triangles.py
@@ -3,15 +3,28 @@ import sys
 class Triangle:
     def __init__(self, side1, side2, side3):
         sides = sorted([side1, side2, side3])
-        self.side_a, self.side_b, self.side_C = self.sides
+        self.side_a, self.side_b, self.side_c = self.sides
     
     def validateSides(self):
         if any(side <= 0 for side in self.sides):
-            return False
+            return (False, "Sides cannot be zero or negative")
         if self.side_a + self.side_b <= self.side_c:
-            return False
-        return True
-
+            return (False, "Triangle is physically invalid")
+        return (True, "")
+    
+    def determineTriangle(self):
+        if self.validateSides[0] is False:
+            return self.validateSides[1]
+        
+        if self.side_a == self.side_b == self.side_c:
+            triangle = "Equilateral"
+        elif self.side_a == self.side_b or self.side_b == self.side_c or self.side_a == self.side_c:
+            triangle = "Isoceles"
+        elif (self.side_a ** 2) + (self.side_b ** 2) == (self.side_c ** 2):
+            triangle = "Right angle"
+        else:
+            triangle = "Scalene"
+        return triangle
 def getSides():
     sides = []
     for num, i in enumerate(range(3)):
