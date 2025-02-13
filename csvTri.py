6e9992c3de35602e83891990d0ca9c2e64cc41af
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-02-13 11:32:15 +1100

    Add list to store new data to write to new csv

--- csvTri.py
+++ csvTri.py
@@ -3,7 +3,16 @@ import csv
 
 with open("tri_sides.csv", "r", encoding='utf-8-sig') as f:
     reader = csv.DictReader(f)
+    newCsvData = []
     for row in reader:
         tri = Triangle(row['side_a'], row['side_b'], row['side_c'])
         print(tri.validtyStatement)
         print(tri.triangleTypeStatement)
+        
+        newCsvData.append({
+            'side_a': row['side_a'],
+            'side_b': row['side_b'],
+            'side_c': row['side_c'],
+            'validity': tri.validtyWord,
+            'triangle_type': tri.triangleType
+        })
