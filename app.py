51ee7a99016989d1ae73119fa4ad267e63a60187
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-02-13 16:40:03 +1100

    Make variable names in flask app more consistent

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template("index.html", triangle_message="Enter triangle information above", triangle_validity="")
+    return render_template("index.html", triangleMessage="Enter triangle information above", triangleValidity="")
 
 @app.route('/send', methods=["GET", "POST"])
 def calc():
     if request.method == "GET":
         return redirect("/")
     else:
-        new = Triangle(request.form.get("side_a"), request.form.get("side_b"), request.form.get("side_c"))
-        return render_template("index.html", triangle_message=new.triangleTypeStatement, triangle_validity=new.validtyStatement)
+        userTriangle = Triangle(request.form.get("side_a"), request.form.get("side_b"), request.form.get("side_c"))
+        return render_template("index.html", triangleMessage=userTriangle.triangleTypeStatement, triangleValidity=userTriangle.validtyStatement)
