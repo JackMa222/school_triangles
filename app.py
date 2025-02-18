cc406bddc05280ecd0e2cc03874c9a4ceadedd6b
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-02-18 11:04:13 +1100

    Update styling and integrate to Flask static files

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template("index.html", triangleMessage="Enter triangle information above", triangleValidity="")
+    return render_template("index.html", triangleMessage="Enter triangle information above", triangleValidity="Trianlge Validity will appear here")
 
 @app.route('/send', methods=["GET", "POST"])
 def calc():
